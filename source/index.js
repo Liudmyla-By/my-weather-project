2c8162c3cc0ce79211cac41143ef119d2a358de7
Liudmyla-By <132687246+Liudmyla-By@users.noreply.github.com>
2023-05-15 14:58:48 +0100

    Added a description

--- source/index.js
+++ source/index.js
@@ -14,6 +14,7 @@ function showWeather(response) {
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;
 }
 
 function search(city) {
